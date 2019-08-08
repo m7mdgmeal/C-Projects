f7d742852818abb75f170237fa8a1cae0c448ab4
m7mdgmeal <m7mdgmeal@gmail.com>
2019-08-08 12:57:36 -0700

    =half phase 1

--- bash.sh
+++ bash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-g++ -ansi -std=c++98 -Wall -Werror -pedantic -Wconversion -c test.cpp
-g++ -ansi -std=c++98 -Wall -Werror -pedantic -Wconversion -o run test.o
+g++ -ansi -std=c++03 -Wall -Werror -pedantic -Wconversion -c my_time.cpp testphase1.cpp
+g++ -ansi -std=c++03 -Wall -Werror -pedantic -Wconversion -o run my_time.o testphase1.o
 valgrind ./run
 
 
