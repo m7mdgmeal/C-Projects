fcb81881ae7e447919209ee9d8efaaa0ed6970b1
m7mdgmeal <m7mdgmeal@gmail.com>
2019-08-12 13:25:57 -0700

    =Done MsgPrinterScg... + comments

--- msg_printer.cpp
+++ msg_printer.cpp
@@ -1,60 +1,64 @@
 #include "msg_printer.h"
 
 //*************************************************************************************************************//
+//*************************************************************************************************************//
 //************************************* MsgPrinter Class ******************************************************//
 //*************************************************************************************************************//
+//*************************************************************************************************************//
 
-MsgPrinter::MsgPrinter(const string &message) : message(message)
+//************************************** Initialization methods ************************************************//
+//ctor that take a const refrencse to string object and initialize the messate with it
+MsgPrinter::MsgPrinter(const string &message) : message(message)  
 {
 }
-
+//ctor char takes char * as a string and initialize the message string
 MsgPrinter::MsgPrinter(const char *message) : message(message)
 {
 }
+//copy ctor that takes const reference to MsgPrinter object and clone to the message the message in the object
 MsgPrinter::MsgPrinter(const MsgPrinter &msgprint) : message(msgprint.get_message())
 {
 }
-
+//dtor that prints the end of the object with the message
 MsgPrinter::~MsgPrinter()
 {
     cout << "End of MsgPrinter: " << this->message << endl;
 }
 
+
+
+//*************************************************************************************************************//
 //*************************************************************************************************************//
 //************************************** MsgPrinterSurrounding Class ******************************************//
 //*************************************************************************************************************//
+//*************************************************************************************************************//
 
+
+
+//*************************************************************************************************************//
+//************************************** Initialization methods ************************************************//
+
+//ctor that takes 1 args, initialize the base class with this argument and initialize the before and after to Null
 MsgPrinterSurrounding::MsgPrinterSurrounding(const string &msg) : MsgPrinter(msg), m_before(NULL), m_after(NULL)
 {
 }
+//ctor that takes 3 args, initialize the base class with this 1st argument and alocate before and after in order to clone the messages(b_msg,a_msg)
 
 MsgPrinterSurrounding::MsgPrinterSurrounding(const string &msg, const char *b_msg, const char *a_msg) : MsgPrinter(msg)
 {
-    clone_msg(b_msg, &(this->m_before));
-    clone_msg(a_msg, &(this->m_after));
+    clone_msg(b_msg, &(this->m_before));     // alocate and clone to member before the b_msg string
+    clone_msg(a_msg, &(this->m_after));      // alocate and clone to member after the a_msg string  
 }
 
+// copy ctor , that initialize the base class with get method , and alocate before and after in order to clone the messages(b_msg,a_msg)
 MsgPrinterSurrounding::MsgPrinterSurrounding(const MsgPrinterSurrounding &copy) : MsgPrinter(copy.get_message())
 {
-    clone_msg(copy.get_before(), &(this->m_before));
-    clone_msg(copy.get_after(), &(this->m_after));
+    clone_msg(copy.get_before(), &(this->m_before));   // alocate and clone to member before the before msg in copy object
+    clone_msg(copy.get_after(), &(this->m_after));     // alocate and clone to member after the after msg in copy object
 }
 
-void MsgPrinterSurrounding::print() const
-{
 
-    this->print_before();
-
-    MsgPrinter::print();
-
-    this->print_after();
-}
-void MsgPrinterSurrounding::clone_msg(const char *base_msg, char **required_msg)
-{
-
-    (*required_msg) = new char[strlen(base_msg) + 1];
-    strcpy((*required_msg), base_msg);
-}
+//dtor deltes the alocated memory and print nice destrunt message + prints the base message
 MsgPrinterSurrounding::~MsgPrinterSurrounding()
 {
     delete[] m_after;
@@ -63,15 +67,43 @@ MsgPrinterSurrounding::~MsgPrinterSurrounding()
     MsgPrinter::print();
 }
 
+
+
+//*************************************************************************************************************//
+//************************************* override methods ******************************************************//
+
+void MsgPrinterSurrounding::print() const                 // override the print function from the base class
+{
+    this->print_before();                                // prints the before msg
+    MsgPrinter::print();                                 // prints the Base message 
+    this->print_after();                                 // prints the after message
+}
+
+
+
+//*************************************************************************************************************//
+//**************************************** private methods ****************************************************//
+
+// take char* as a string , and point to char* , alocate the pointer to the size of the string and copy the contant .
+void MsgPrinterSurrounding::clone_msg(const char *base_msg, char **required_msg)
+{
+    (*required_msg) = new char[strlen(base_msg) + 1];
+    strcpy((*required_msg), base_msg);
+}
+
+
+//*************************************************************************************************************//
+    //****************************** operators *************************************//
+    // operator =
 MsgPrinterSurrounding &MsgPrinterSurrounding::operator=(const MsgPrinterSurrounding & msg_p_s)
 {
-    delete[] this->m_after;
+    delete[] this->m_after;                      // delete the alocated memory 
     delete[] this->m_before;
 
     
-    this->set_message(msg_p_s.get_message());
+    this->set_message(msg_p_s.get_message());   // set the base message as the msg_p_s object
 
-    clone_msg(msg_p_s.get_before(), &(this->m_before));
-    clone_msg(msg_p_s.get_after(), &(this->m_after));
-    return *this;
+    clone_msg(msg_p_s.get_before(), &(this->m_before));   // alocate + clone to before msg
+    clone_msg(msg_p_s.get_after(), &(this->m_after));       // alocate + clone to after msg
+    return *this;                                           // return *this 
 }
--- msg_printer.h
+++ msg_printer.h
@@ -3,28 +3,38 @@
 
 #include <iostream>
 #include <string>
-#include <new>
+#include <new>         
 #include <string.h>
 
 using namespace std;
 
-//*************************************************************************************************************//
-//************************************* MsgPrinter Class ******************************************************//
-//*************************************************************************************************************//
+//***********************************************************************************//
+//************************* MsgPrinter Class ****************************************//
+//***********************************************************************************//
 
 class MsgPrinter
 {
+
 private:
+    //**************************** private members **********************************//
+
     string message; // a const string that holds the message string
+
 public:
+    //**************************** public methods ***********************************//
+
+    //*************************** Intialization methods *****************************//
     MsgPrinter(const string &message); // ctor that takes string as a string object
     MsgPrinter(const char *message);   // ctor that takes string as a char * object
     MsgPrinter(const MsgPrinter &);    // c.ctor
+    //**************************** destruction method *******************************//
     virtual ~MsgPrinter();             // dtor
+    //**************************** getters & setters ********************************//
+    const string get_message() const;  //gets the message as a string
+    void set_message(const string &);  // set string to message 
 
-    const string get_message() const;
+    //****************************** class methods **********************************//
     virtual void print() const; // virtual print function , print the message
-    void set_message(const string &);
 };
 
 //*************************************************************************************************************//
@@ -33,69 +43,96 @@ public:
 //*************************************************************************************************************//
 //************************************** MsgPrinterSurrounding Class ******************************************//
 //*************************************************************************************************************//
-
+// inherte from MsgPrinter a public inhertence
 class MsgPrinterSurrounding : public MsgPrinter
 {
 private:
-    char *m_before;
-    char *m_after;
-    void clone_msg(const char *base_msg, char **required_msg);
-    
+
+    //**************************** private members **********************************//
+
+    char *m_before;                                                     // a string that have the before message
+    char *m_after;                                                      // a string that have the after message
+    void clone_msg(const char *base_msg, char **required_msg);          // private funtion that alocate and clone strings
+
+    //*******************************************************************************//
+
 protected:
     virtual void print_before() const;
     void print_after() const;
 
 public:
-    MsgPrinterSurrounding(const string &);
-    MsgPrinterSurrounding(const string &, const char *, const char *);
-    MsgPrinterSurrounding(const MsgPrinterSurrounding &);
-    ~MsgPrinterSurrounding();
+    //**************************** public methods ***********************************//
 
-    MsgPrinterSurrounding & operator=(const MsgPrinterSurrounding & ); 
+    //*************************** Intialization methods *****************************//
+    MsgPrinterSurrounding(const string &);                              // ctor with 1 arguments, that iniatilaze the Base class with this argument 
+                                                                        //and nulls the before and after
+
+    MsgPrinterSurrounding(const string &, const char *, const char *);  // ctor with 3 args, that intialize the Base class , before and after
+    MsgPrinterSurrounding(const MsgPrinterSurrounding &);               // c.ctor that intialize the Base class , before and after
+    ~MsgPrinterSurrounding();    
+                                                                        // dtor , that deletes before and after and destruct the object.
+    
+    //****************************** operators *************************************//
+    MsgPrinterSurrounding &operator=(const MsgPrinterSurrounding &);
+
+
+    //****************************** getters ***************************************//
+
+    const char *get_after() const;
+    const char *get_before() const;
+
+    //****************************** override **************************************//
+
     void print() const;
-    const char* get_after() const;
-    const char* get_before() const;
+
+
 };
 
-//********************************** inlines MsgPrinterSurrounding Class **************************************//
-
-//*************************************************************************************************************//
-//*************************************************************************************************************//
-
 #endif
+//*************************************************************************************************************//
+//*************************************************************************************************************//
+//*************************************************************************************************************//
 
+
+//*************************************************************************************************************//
 //************************************* inlines MsgPrinter Class **********************************************//
+//*************************************************************************************************************//
 
-inline void MsgPrinter::print() const
+inline void MsgPrinter::print() const                                   // prints the message 
 {
     cout << "Message: " << this->message << endl;
 }
 
-inline const string MsgPrinter::get_message() const
+inline const string MsgPrinter::get_message() const                     // gets the message
 {
     return this->message;
 }
 
-//********************************** inlines MsgPrinterSurrounding Class **************************************//
+inline void MsgPrinter::set_message(const string &msg)                   // set the message with msg object
+{
+    this->message = msg;
+}
 
-inline void MsgPrinterSurrounding::print_before() const
+//*************************************************************************************************************//
+//********************************** inlines MsgPrinterSurrounding Class **************************************//
+//*************************************************************************************************************//
+
+inline void MsgPrinterSurrounding::print_before() const                 // prints the before string , if its null --> prints null
 {
     this->m_before == NULL ? cout << "NULL" << endl : cout << this->m_before << endl;
 }
-inline void MsgPrinterSurrounding::print_after() const
+
+inline void MsgPrinterSurrounding::print_after() const                  // prints the after string , if its null --> prints null
 {
     this->m_after == NULL ? cout << "NULL" << endl : cout << this->m_after << endl;
 }
-inline const char * MsgPrinterSurrounding::get_after() const
+
+inline const char *MsgPrinterSurrounding::get_after() const             // gets the after string(message)
 {
     return this->m_after;
 }
-inline const char* MsgPrinterSurrounding::get_before() const
+
+inline const char *MsgPrinterSurrounding::get_before() const            //gets the before string(message)
 {
     return this->m_before;
-}
-
-inline void MsgPrinter::set_message(const string & msg)
-{
-    this->message=msg;
 }
